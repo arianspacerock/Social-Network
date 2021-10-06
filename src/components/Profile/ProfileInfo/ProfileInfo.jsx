1d33fd8cb134dd40514dbd1a8d162554072ed597
arianspacerock <siropemptyspace@gmail.com>
2021-10-06 13:20:36 +0300

    refactor: change structure of the project

---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import MyPosts from "./MyPosts/MyPosts";
+import s from "./Profile.module.css";
+
+const Profile = () => {
+    return (
+        <div>
+            <ProfileInfo />
+            <div>
+                <img
+                    src="https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg"
+                    width="800px" height="300px">
+                </img>
+            </div>
+            <div>ava + description</div>
+            <MyPosts />
+        </div>
+    );
+};
+
+export default Profile;
